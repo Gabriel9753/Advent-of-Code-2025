cf2643c409c8a572e23fdf6a3ddb2de47bc81435
Gabriel Schurr <gabriel.elias2000@outlook.de>
2025-12-09 16:21:08 +0100

    Add first solution for day 9

---
+++ src/day_09/solution.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+import re
+import sys
+from bisect import bisect_left, bisect_right, insort
+from collections import OrderedDict, defaultdict, deque, namedtuple
+from datetime import datetime
+from functools import lru_cache, partial
+from heapq import heapify, heappop, heappush
+from itertools import chain, combinations, permutations, product
+from math import ceil, floor, gcd, inf, lcm, log2, sqrt
+from typing import Dict, List, Set, Tuple
+
+import numpy as np
+import z3
+from more_itertools import chunked, windowed
+from rich import print
+from shapely.geometry import Polygon, box
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+par_dir = os.path.dirname(cur_dir)
+sys.path.append(par_dir)
+
+from util.general_util import average_time, load_input, timer, write_times_to_readme
+
+sys.setrecursionlimit(5000)
+
+last_dir = str(os.path.basename(os.path.normpath(cur_dir)))
+cur_day = re.findall(r"\d+", last_dir)
+cur_day = int(cur_day[0]) if len(cur_day) > 0 else datetime.today().day
+images_path = os.path.join(par_dir, "images")
+
+
+@timer(return_time=True)
+def preprocess_input(input_data):
+    return [np.array(list(map(int, x.split(",")))) for x in input_data.splitlines()]
+
+
+@timer(return_time=True)
+def task1(day_input):
+    max_area = -np.inf
+    max_points = []
+
+    for p1, p2 in combinations(day_input, 2):
+        area = np.prod(abs(p1 - p2) + 1)
+        if area > max_area:
+            max_area = area
+            max_points = [p1, p2]
+
+    print(f"{max_points} | dist={max_area}")
+
+    return max_area
+
+
+# 340007664 X
+# 2165426818 X
+# 102205235 X
+# 2338426344 X too low
+# 4781090232 X too low
+# 4782268188
+
+
+@timer(return_time=True)
+def task2(day_input):
+    polygon = Polygon(day_input)
+    hist = []
+
+    for p1, p2 in combinations(day_input, 2):
+        area = np.prod(abs(p1 - p2) + 1)
+        hist.append([p1, p2, area])
+
+    hist = sorted(hist, key=lambda x: x[2], reverse=True)
+
+    for p1, p2, area in hist:
+        rect = box(*[min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1])])
+        if polygon.covers(rect):
+            return area
+
+    # return max_area
+
+
+def main(args):
+    if not args.final:
+        day_input = load_input(os.path.join(cur_dir, "example_input.txt"))
+    else:
+        day_input = load_input(os.path.join(cur_dir, "input.txt"))
+
+    day_input, t = preprocess_input(day_input)
+    result_task1, time_task1 = task1(day_input)
+    result_task2, time_task2 = task2(day_input)
+
+    print(f"\nDay {cur_day}")
+    print("------------------")
+    print(f"Processing data: {t:.6f} seconds")
+    print(f"Task 1: {result_task1} ({time_task1:.6f} seconds)")
+    print(f"Task 2: {result_task2} ({time_task2:.6f} seconds)")
+
+    if args.timeit:
+        avg_time_task1 = average_time(100, task1, [], day_input)
+        avg_time_task2 = average_time(100, task2, [], day_input)
+        print("\nAverage times:")
+        print(f"Task 1: {avg_time_task1:.6f} seconds")
+        print(f"Task 2: {avg_time_task2:.6f} seconds")
+        write_times_to_readme(cur_day, avg_time_task1, avg_time_task2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--final", help="Use the final input", action="store_true")
+    parser.add_argument("--timeit", help="Average the execution time over 100 runs", action="store_true")
+    main(parser.parse_args())
